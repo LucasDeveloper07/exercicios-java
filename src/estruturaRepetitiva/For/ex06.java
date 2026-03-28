ab138257104143c7a80f3d0f9650f5c65be7a9bc
Lucas Felipe <lucas.repensando435@gmail.com>
2026-03-28 16:01:06 -0300

    Novos exercícios de estrutura repetitiva

---
+++ src/estruturaRepetitiva/For/ex06.java
@@ -0,0 +1,23 @@
+package estruturaRepetitiva.For;
+import java.util.Scanner;
+
+public class ex06 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int num1;
+
+        System.out.println("Digite um número inteiro: ");
+        num1 = sc.nextInt();
+
+        for (int i = 1; i <= num1; i++) {
+            if (num1 % i == 0) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
