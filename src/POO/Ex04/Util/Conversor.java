c66ec85cd1c5ca4723c3c2adbeb23f533b987554
Lucas Felipe <lucas.repensando435@gmail.com>
2026-04-12 19:07:53 -0300

    POO

--- src/POO/Ex04/Util/Conversor.java
+++ src/POO/Ex04/Util/Conversor.java
@@ -2,10 +2,11 @@ package POO.Ex04.Util;
 
 public class Conversor {
     
-    public static double IOF = 0.06;
+    public static final double IOF = 0.06;
 
     public static double compraDolar(double dolar, double compra) {
 
-        return ((compra * dolar) + ((compra * dolar) * IOF));
+        double total = compra * dolar;
+        return total +  (total * IOF);
     }
 }
